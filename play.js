ab53906732ae65d226b7fe10b53bb5318810d5a6
BoxinCao <ngxmex@gmail.com>
2025-04-12 14:35:25 -0400

    Update play.js

--- play.js
+++ play.js
@@ -1,29 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '172.25.96.1',// IP address here,
-    port: 50541// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("Successfully connected!");
-  });
-
-  // ✅ Listen for incoming data from the server
-  conn.on("data", (message) => {
-    console.log("!!!", message);
-  });
-
-
-
-
-  return conn;
-};
+const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
