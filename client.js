269b57199aa15b8b605c8d17a21ce00f05cafcab
BoxinCao <ngxmex@gmail.com>
2025-04-12 14:35:00 -0400

    Create client.js

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '172.25.96.1',// IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected!");
+  });
+
+  // ✅ Listen for incoming data from the server
+  conn.on("data", (message) => {
+    console.log("!!!", message);
+  });
+
+
+
+
+  return conn;
+};
+
+module.exports = connect;
